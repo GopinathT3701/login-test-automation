892789a883a2c7bc461325fbefd6b8bf9269947a
GopinathT3701 <gopinath.t@c1exchange.com>
2025-08-06 11:38:22 +0530

    rename

--- src/test/java/com/automation/Junit/Junit_basic.java
+++ src/test/java/com/automation/Junit/Junit_basic.java
@@ -3,11 +3,15 @@ package com.automation.Junit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class GoogleTest {
+public class Junit_basic {
 
     // Sample methods
     int add(int a, int b) {
         return a + b;
+    }
+    
+    int subtract(int a, int b) {
+    	return a - b;
     }
 
     int multiply(int a, int b) {
@@ -20,6 +24,11 @@ public class GoogleTest {
         Assertions.assertEquals(5, add(2, 3), "2 + 3 should equal 5");
     }
 
+    @Test
+    void testSubtraction() {
+        Assertions.assertEquals(7, subtract(12, 5), "12 - 5 should equal 7");
+    }
+    
     @Test
     void testMultiplication() {
         Assertions.assertEquals(12, multiply(3, 4), "3 * 4 should equal 12");
